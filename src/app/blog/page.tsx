7ee1b3d6552745f93f22eb56fe9808d4505b1ef2
Ha Nguyen B <ngbha2003@gmail.com>
2024-05-01 21:32:25 +0700

    nextjs tailwind setup, first pass at navbar

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from "react";
+import Navbar from "../components/navbar";
+
+
+export default function Blog() {
+    return (
+      <div className="text-2xl">
+        <Navbar/>
+      </div>
+    );
+  }
+  
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from "react";
+import Navbar from "./components/navbar";
+
+export default function Home() {
+  return (
+    <div className="text-2xl">
+      <Navbar/>
+    </div>
+
+  );
+}
